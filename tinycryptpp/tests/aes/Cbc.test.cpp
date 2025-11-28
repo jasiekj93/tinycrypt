a7802d86b483a46820a66cc4e5f90efe2ee6288a
Adrian Szczepanski <aszczepanski@nodomain.pl>
2025-11-28 15:01:10 +0100

    feat: test czyszczenia Aes

--- tinycryptpp/tests/aes/Cbc.test.cpp
+++ tinycryptpp/tests/aes/Cbc.test.cpp
@@ -47,3 +47,32 @@ TEST(AesCbcTest, encrypt_and_Decrypt)
     CHECK_TRUE(decryptResult.has_value());
     STRCMP_EQUAL(plaintext.data(), reinterpret_cast<const char*>(decryptResult->data()));
 }
+
+TEST(AesCbcTest, cleanAfterUse)
+{
+    etl::array<uint8_t, 128> encryptBuffer = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    {
+        Key key = {
+            0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+            0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+        };
+
+        Iv iv = {
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+        };
+
+        etl::string<64> plaintext = "This is a test message for AES CBC mode encryption!";
+        etl::span<const uint8_t> plaintextSpan(
+            reinterpret_cast<const uint8_t*>(plaintext.data()), plaintext.max_size());
+
+        Encrypt encryptor(key, iv, { encryptBuffer.data(), encryptBuffer.size() });
+        auto encryptResult = encryptor(plaintextSpan);
+
+        CHECK_TRUE(encryptResult.has_value());
+    }
+
+    for (auto byte : encryptBuffer)
+        CHECK_EQUAL(0, byte);
+}
